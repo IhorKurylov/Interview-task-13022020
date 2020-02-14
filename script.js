e3c0bdf4678049d146697dbc30ae1909e13707f8
Ihor <kuryloff@gmail.com>
2020-02-14 22:42:28 +0200

    finished JS no conditions for inputs

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let getId = (x) => document.getElementById(x);
-const getClass = (x) => document.getElementsByClassName(x);
-const modalButton = document.getElementById('modalOpenBtn');
-
+let resultRow0;
+let resultRow1;
+let resultRow2;
 // changes background color
 for (let i = 0; i <= 2; i++) {
     getId(`input${i}`).addEventListener("click", () => {
@@ -9,20 +9,31 @@ for (let i = 0; i <= 2; i++) {
         else getId(`row${i}`).style.backgroundColor = '#ffffff';
     })
 };
-
-
-
-
-
-
-
-
-
-
-
-getId("modalOpenBtn").addEventListener("click", () => {
-    getId('modalOpenBtn').innerText = 'Close calc'
-});
-getId('modalWindow').addEventListener('click', () => {
-    getId('modalOpenBtn').innerText = 'Open calc'
-});
+const resultApply = (a, b, c) => {
+    getId(`result0`).innerText = a;
+    getId(`result1`).innerText = b;
+    getId(`result2`).innerText = c;
+};
+//Summary for 3 inputs
+//no conditions for NaN or null and in inputs values no error <span>
+const sum = () => {
+    resultRow0 = parseInt(getId('row10').value) + parseInt(getId('row11').value) + parseInt(getId('row12').value);
+    resultRow1 = parseInt(getId('row20').value) + parseInt(getId('row21').value) + parseInt(getId('row22').value);
+    resultRow2 = parseInt(getId('row30').value) + parseInt(getId('row31').value) + parseInt(getId('row32').value);
+    resultApply(resultRow0, resultRow1, resultRow2);
+};
+//multiply for 3 inputs
+//no conditions for NaN or null and in inputs values no error <span>
+const multiply = () => {
+    resultRow0 = parseInt(getId('row10').value) * parseInt(getId('row11').value) * parseInt(getId('row12').value);
+    resultRow1 = parseInt(getId('row20').value) * parseInt(getId('row21').value) * parseInt(getId('row22').value);
+    resultRow2 = parseInt(getId('row30').value) * parseInt(getId('row31').value) * parseInt(getId('row32').value);
+    resultApply(resultRow0, resultRow1, resultRow2);
+};
+//sum event
+getId('sum').addEventListener('click', () => sum());
+//multiply event
+getId('multiply').addEventListener('click', () => multiply())
+//modal open Button text edit
+getId("modalOpenBtn").addEventListener("click", () => getId('modalOpenBtn').innerText = 'Close calc');
+getId('modalWindow').addEventListener('click', () => getId('modalOpenBtn').innerText = 'Open calc');
